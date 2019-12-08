0547fd717e2cd3a71681a811ab247bcbba328c5b
Elana Hashman <ehashman@debian.org>
2019-12-07 21:49:21 -0800

    Solve problem 1!

--- aoc/d01.scm
+++ aoc/d01.scm
@@ -1,7 +1,20 @@
+#!/usr/bin/guile
+!#
+
 ;; AoC 2019 Day 1
 
 (define-module (aoc d01)
   #:export (fuel-required))
 
+(use-modules (ice-9 rdelim))
+
 (define (fuel-required mass)
   (- (quotient mass 3) 2))
+
+(set-current-input-port (open-input-file "input/d01"))
+(let lp ((mod-mass (read-line))
+         (total-weight 0))
+  (if (eof-object? mod-mass)
+    (begin (display total-weight) (newline))
+    (lp (read-line)
+        (+ total-weight (fuel-required (string->number mod-mass))))))
