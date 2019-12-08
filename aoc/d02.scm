a3b04a50b3d5c9db6f8f58b8e8bf227b3d9f92e5
Elana Hashman <ehashman@debian.org>
2019-12-07 22:59:42 -0800

    Execute some instructions

--- aoc/d02.scm
+++ aoc/d02.scm
@@ -4,11 +4,29 @@
 ;; AoC 2019 Day 2
 
 (define-module (aoc d02)
-  #:export (parse-program))
+  #:export (parse-program
+            eval-inst
+            run-inst))
 
 (use-modules (ice-9 rdelim))
 
-(define (parse-program program)
-  (list->vector (map string->number (string-split program #\,))))
+(define (parse-program p)
+  (list->vector (map string->number (string-split p #\,))))
+
+(define (eval-inst i p f)
+  (begin
+    (let* ((l1  (vector-ref p (+ i 1)))
+           (l2  (vector-ref p (+ i 2)))
+           (dst (vector-ref p (+ i 3)))
+           (v1  (vector-ref p l1))
+           (v2  (vector-ref p l2)))
+      (vector-set! p dst (f v1 v2)))
+    #t))
+
+(define (run-inst i p)
+  (case (vector-ref p i)
+    ((1) (eval-inst i p +))
+    ((2) (eval-inst i p *))
+    ((99) #f)))
 
 (set-current-input-port (open-input-file "input/d02"))
--- test/02.scm
+++ test/02.scm
@@ -10,4 +10,16 @@
 (test-equal (parse-program "1,1,1,4,99,5,6,0,99")
             #(1 1 1 4 99 5 6 0 99))
 
+(define program #(1 9 10 3 2 3 11 0 99 30 40 50))
+
+(define p (vector-copy program))
+
+; i = 0, opcode is 1, add
+(test-eq (eval-inst 0 p +) #t)
+(test-equal p #(1 9 10 70 2 3 11 0 99 30 40 50))
+
+; i = 4, opcode is 2, multiply
+(test-eq (eval-inst 4 p *) #t)
+(test-equal p #(3500 9 10 70 2 3 11 0 99 30 40 50))
+
 (test-end "puzzle-1")
