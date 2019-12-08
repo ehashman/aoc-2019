6cf8b8e005aa3345fe852e30b70ac4d4418f5009
Elana Hashman <ehashman@debian.org>
2019-12-08 00:23:25 -0800

    Solve day 2 problem 2!

--- aoc/d02.scm
+++ aoc/d02.scm
@@ -3,6 +3,8 @@
 
 ;; AoC 2019 Day 2
 
+;; Problem 1
+
 (define-module (aoc d02)
   #:export (parse-program
             eval-inst
@@ -35,5 +37,24 @@
       ((eqv? i -1) p)))
 
 (set-current-input-port (open-input-file "input/d02"))
-(display (run-program (parse-program (read-line))))
+(define gravity-assist (parse-program (read-line)))
+(display (run-program (vector-copy gravity-assist)))
+(newline)
+
+
+;; Problem 2
+
+(define (check-inputs n v)
+  (let ((p (vector-copy gravity-assist)))
+    (vector-set! p 1 n)
+    (vector-set! p 2 v)
+    (eqv? (vector-ref (run-program p) 0) 19690720)))
+
+;; who needs to break early, meh
+(do ((n 0 (1+ n)))
+    ((eqv? n 100))
+  (do ((v 0 (1+ v)))
+      ((eqv? v 100))
+    (if (check-inputs n v)
+      (display (+ (* n 100) v)))))
 (newline)
