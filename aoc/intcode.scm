b3b21425c35790510bcee7a32fbfbdbcc691f6d2
Elana Hashman <ehashman@debian.org>
2019-12-08 13:14:52 -0800

    Implement jumps and add many tests

--- aoc/intcode.scm
+++ aoc/intcode.scm
@@ -15,6 +15,12 @@
 (define (normalize-bool b)
   (if b 1 0))
 
+;; Just support a pair of modes for now
+(define (modes o)
+  (let ((m (quotient o 100)))
+    (cons (remainder m 10)
+          (remainder (quotient m 10) 10))))
+
 (define (get-value mode p l)
   (if (eqv? mode 1)
     l
@@ -27,9 +33,9 @@
   (let* ((l1  (vector-ref p (+ i 1)))
          (l2  (vector-ref p (+ i 2)))
          (dst (vector-ref p (+ i 3)))
-         (modes (quotient (vector-ref p i) 100))
-         (m1  (remainder modes 10))
-         (m2  (remainder (quotient modes 10) 10))
+         (ms  (modes (vector-ref p i)))
+         (m1  (car ms))
+         (m2  (cdr ms))
          (v1  (get-value m1 p l1))
          (v2  (get-value m2 p l2)))
     (vector-set! p dst (f v1 v2))
@@ -43,14 +49,23 @@
 
 (define (write-output i p)
   (let* ((dst (vector-ref p (+ i 1)))
-         (modes (quotient (vector-ref p i) 100))
-         (m1  (remainder modes 10)))
+         (ms  (modes (vector-ref p i)))
+         (m1  (car ms)))
     (display (get-value m1 p dst))
     (newline)
     (+ i 2)))
 
 (define (jump i p pred)
-  i)
+  (let* ((l1 (vector-ref p (+ i 1)))
+         (l2 (vector-ref p (+ i 2)))
+         (ms (modes (vector-ref p i)))
+         (m1 (car ms))
+         (m2 (cdr ms))
+         (v1 (get-value m1 p l1))
+         (v2 (get-value m2 p l2)))
+    (if (pred v1)
+      v2
+      (+ i 3))))
 
 (define (run-inst i p)
   (case (remainder (vector-ref p i) 100)
@@ -58,7 +73,7 @@
     ((2) (eval-inst i p *))
     ((3) (get-input i p))
     ((4) (write-output i p))
-    ((5) (jump i p (not zero?)))
+    ((5) (jump i p (negate zero?)))
     ((6) (jump i p zero?))
     ((7) (eval-inst i p (compose normalize-bool <)))
     ((8) (eval-inst i p (compose normalize-bool eqv?)))
--- test/intcode.scm
+++ test/intcode.scm
@@ -23,6 +23,7 @@
 (test-eq (eval-inst 4 p *) 8)
 (test-equal p #(3500 9 10 70 2 3 11 0 99 30 40 50))
 
+; test sample programs
 (set! p (vector 1 0 0 0 99))
 (run-program p)
 (test-equal p #(2 0 0 0 99))
@@ -73,5 +74,35 @@
 (write-output 0 #(104 666 99))
 (set-current-output-port stdout)
 (test-equal (get-output-string my-output) "666\n")
+
+; more sample programs
+(define (test-program-with-io p input output)
+  (set-current-input-port (open-input-string input))
+  (set! my-output (open-output-string))
+  (set-current-output-port my-output)
+  (run-program p)
+  (set-current-output-port stdout)
+  (test-equal (get-output-string my-output) output))
+
+(test-program-with-io (vector 3 9 8 9 10 9 4 9 99 -1 8) "8" "1\n")
+(test-program-with-io (vector 3 9 8 9 10 9 4 9 99 -1 8) "80" "0\n")
+(test-program-with-io (vector 3 9 7 9 10 9 4 9 99 -1 8) "3" "1\n")
+(test-program-with-io (vector 3 9 7 9 10 9 4 9 99 -1 8) "8" "0\n")
+(test-program-with-io (vector 3 3 1108 -1 8 3 4 3 99) "8" "1\n")
+(test-program-with-io (vector 3 3 1108 -1 8 3 4 3 99) "80" "0\n")
+(test-program-with-io (vector 3 3 1107 -1 8 3 4 3 99) "3" "1\n")
+(test-program-with-io (vector 3 3 1107 -1 8 3 4 3 99) "8" "0\n")
+(test-program-with-io (vector 3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9) "8" "1\n")
+(test-program-with-io (vector 3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9) "0" "0\n")
+(test-program-with-io (vector 3 3 1105 -1 9 1101 0 0 12 4 12 99 1) "8" "1\n")
+(test-program-with-io (vector 3 3 1105 -1 9 1101 0 0 12 4 12 99 1) "0" "0\n")
+
+(define long-p #(3 21 1008 21 8 20 1005 20 22 107 8 21 20 1006 20 31
+                 1106 0 36 98 0 0 1002 21 125 20 4 20 1105 1 46 104
+                 999 1105 1 46 1101 1000 1 20 4 20 1105 1 46 98 99))
+(test-program-with-io (vector-copy long-p) "5" "999\n")
+(test-program-with-io (vector-copy long-p) "8" "1000\n")
+(test-program-with-io (vector-copy long-p) "20" "1001\n")
+
 
 (test-end "intcode-checker")
