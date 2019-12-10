38fcfba8279105a1e4fe3079ebea5caf604e4745
Elana Hashman <ehashman@debian.org>
2019-12-09 19:30:59 -0800

    Solve day 7 puzzle 1!

--- aoc/d07.scm
+++ aoc/d07.scm
@@ -24,6 +24,39 @@
     (run-program prog)
     (if (null? (cdr phases))
       (begin (set-current-output-port stdout) (get-output-string out))
-      (amplifier-chain p (cdr phases) (string-trim (get-output-string out))))))
+      (amplifier-chain p (cdr phases) (get-output-string out)))))
 
-(define amplifer-program (parse-program (read-line (open-input-file "input/d07"))))
+(define amplifier-program (parse-program (read-line (open-input-file "input/d07"))))
+
+;; There is no stdlib for combinatorics?! Lift it from RosettaCode I guess...
+;; https://rosettacode.org/wiki/Permutations#Scheme
+
+(define (insert l n e)
+  (if (= 0 n)
+      (cons e l)
+      (cons (car l)
+            (insert (cdr l) (- n 1) e))))
+
+(define (seq start end)
+  (if (= start end)
+      (list end)
+      (cons start (seq (+ start 1) end))))
+
+(define (permute l)
+  (if (null? l)
+      '(())
+      (apply append (map (lambda (p)
+                           (map (lambda (n)
+                                  (insert p n (car l)))
+                                (seq 0 (length p))))
+                         (permute (cdr l))))))
+;;; /RosettaCode
+
+(define sequences (permute (list "0" "1" "2" "3" "4")))
+
+(define (run-amps phases)
+  (amplifier-chain amplifier-program phases "0"))
+
+(display
+  (apply max (map (compose string->number string-trim-right run-amps) sequences)))
+(newline)
