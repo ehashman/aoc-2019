3a5c0707382ec63ded969eba295dba9e615270ba
Elana Hashman <ehashman@debian.org>
2019-12-10 22:39:02 -0800

    Add day 9 input

---
+++ aoc/d09.scm
@@ -0,0 +1,16 @@
+#!/usr/bin/guile \
+-L .
+!#
+
+;; AoC 2019 Day 9
+
+;; Problem 1
+
+(define-module (aoc d09))
+
+(use-modules (ice-9 rdelim)
+             (aoc intcode))
+
+(define p (parse-program (read-line (open-input-file "input/d09"))))
+(set-current-input-port (open-input-string "1"))
+(run-program p)
